de662f7a8c86f7d0dfe22097e2f4f1392844c83a
Руслан <fantom203232@gmail.com>
2025-02-13 17:22:27 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,59 @@
 import requests
 from bs4 import BeautifulSoup
+from flask import (
+    request,
+    redirect,
+    render_template,
+    Flask
+)
 
-link = "https://www.mechta.kz/section/smartfony/"
+app = Flask(__name__)
+
+link = "https://www.olx.kz/transport/legkovye-avtomobili/kar/"
 
 response = requests.get(link).text
+
+
 soup = BeautifulSoup(response, 'lxml')
 
-items = soup.find('div', class_='tw-grid tw-grid-cols-3 tw-gap-6 tw-mt-5')
-title = items.find_all('div', class_='tw-mt-[15px] tw-font-normal tw-text-sm tw-text-color1 dark:tw-text-dColor1 tw-overflow-hidden tw-text-ellipsis tw-whitespace-nowrap')
-price = items.find_all('div', class_='tw-text-xl tw-font-bold tw-text-color1 dark:tw-text-dColor1')
+main_div = soup.find('div', class_='css-j0t2x2')
 
-for i in range(len(title)):
-    with open('text.txt', 'w', encoding='utf-8') as file:
-        file.write(f'{title[i].text} - {price[i].text}\n')
+title_div = main_div.find_all('h4', class_='css-1sq4ur2')
+
+price_div = main_div.find_all('p', class_='css-6j1qjp')
+
+import re
+
+def calc(price_div):
+    prices = []
+    for price in price_div:
+        price_text = price.text
+        price_value = re.sub(r'[^\d]', '', price_text)
+        if price_value:
+            prices.append(int(price_value))
+    
+    if prices:
+        return sum(prices) / len(prices)
+    return 0
+
+
+
+
+@app.route("/", methods=["GET", "POST"])
+def get_home():
+    if request.method == 'POST':
+        average_price = calc(price_div)
+        return render_template('index.html', title_div=title_div, price_div=price_div, average_price=average_price)
+    return render_template("index.html")
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
